a6f6dca6cbfa49c9f28852e8988e6985dcd172f8
Mario <blackheaven0305@gmail.com>
2026-02-01 15:21:34 +0100

    first commit POM

---
+++ src/main/java/org/mario/authservice/authservice/configuration/FeignClientInterceptor.java
@@ -0,0 +1,11 @@
+package org.mario.authservice.authservice.configuration;
+
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+public class FeignClientInterceptor {
+}
